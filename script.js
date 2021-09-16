ecf7bd5fbd1a8eea6397f0ca2c940f5599fe89a7
Zachary Elliott <zacharyberdell@gmail.com>
2021-09-15 21:19:03 -0600

    added basic layout to js

---
+++ script.js
@@ -0,0 +1,13 @@
+var wordStorage = [];
+
+function startGame() {
+    //Includes everything neccessary to run the game
+}
+
+function resetScore() {
+    //resets the score when button is clicked
+}
+
+function timer() {
+    //Function concerning the timer
+}
